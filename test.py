882cc4746e7bbd473fa4d599b7bb650c8b3f82dd
Srishti Guleria <61888364+sg7801@users.noreply.github.com>
2021-12-05 08:42:05 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,21 +5,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.image import imread
 from PIL import Image as im
-
-def reconstruction(*args):
-	final_output = average_face
-	for k in range(0,args[0]):
-		weight = np.dot(imVector, eigenVectors[k])
-		final_output = final_output + eigen_face[k] * weight
-	disp(im, final_output)
     
-def disp(x, y):
-	final_output = np.hstack((x,y))	
-	final_output = cv2.resize(final_output, (0,0), fx=3, fy=3)
-	cv2.imshow("Result", final_output)
-
-    
-if __name__ == '__main__':
+def print_test():
 	dataset_path = 'test/'
 	dataset_dir  = os.listdir(dataset_path)
 	width  = 178
@@ -28,17 +15,17 @@ if __name__ == '__main__':
 	testing_tensor   = np.ndarray(shape=(len(test_image_names), height*width), dtype=np.float64)
     
     # Print the test images
-	print("Input Images are as follows: ")
+	print("Printed the Input Images. Check the output window for final results.\n")
 	for i in range(len(test_image_names)):
 		img = imread(dataset_path + test_image_names[i])
 		plt.subplot(3,6,1+i)
 		plt.title(test_image_names[i].split('.')[0][-2:]+test_image_names[i].split('.')[1])
 		plt.imshow(img, cmap='gray')
-		plt.subplots_adjust(right=1.2, top=1.2)
+		plt.subplots_adjust(right=1.000, top=1.000)
 		plt.tick_params(labelleft='off', labelbottom='off', bottom='off',top='off',right='off',left='off', which='both')
 	plt.show()
     
-    # show eigen faces for specific image
+def print_eigen():  
 	neutral = []
 	for i in range(9):
 		i+=1
@@ -49,20 +36,38 @@ if __name__ == '__main__':
 	faces_matrix = np.vstack(neutral)
 	mean_face = np.mean(faces_matrix, axis=0)
 	plt.imshow(mean_face.reshape(49,58),cmap='gray'); 
+	print('Printed Mean Face. Check the output window for final results.\n ')    
 	plt.title('Mean Face')  
+	plt.show()
+
+    
+    #print 5 eigen faces    
 	faces_norm = faces_matrix - mean_face ### normalization
 	faces_norm = faces_norm.T
-    
 	face_cov = np.cov(faces_norm)
 	eigen_vecs, eigen_vals, _ = np.linalg.svd(faces_norm)
-	fig, axs = plt.subplots(1,3,figsize=(15,5))
+
+
 	for i in np.arange(5):
-		ax = plt.subplot(2,5,i+1)
 		img = eigen_vecs[:,i].reshape(49,58)
 		plt.imshow(img, cmap='gray')
-	fig.suptitle("First 5 Eigenfaces", fontsize=16)
+		print('Printed Eigen Face ',i+1,' on the output window\n ')
+		plt.title('Eigen Face :')  
+		plt.show()
     
-
+def recons():
+	def reconstruction(*args):
+		final_output = average_face
+		for k in range(0,args[0]):
+			weight = np.dot(imVector, eigenVectors[k])
+			final_output = final_output + eigen_face[k] * weight
+		disp(im, final_output)
+        
+	def disp(x, y):
+		final_output = np.hstack((x,y))	
+		final_output = cv2.resize(final_output, (0,0), fx=3, fy=3)
+		cv2.imshow("Result", final_output)
+        
     # Read the model from pcaparameters.yml
 	model = "pca_parameters.yml"
 	print("The Model is being Read from pca_parameters.yml", flush=True)
@@ -84,13 +89,15 @@ if __name__ == '__main__':
     
 	eigen_face = [] 
     
-#obtain eigenfaces 
+    #obtain eigenfaces 
 	for eigenVector in eigenVectors:
 		eigenFace = eigenVector.reshape(sz)
 		eigen_face.append(eigenFace) 
-        
-# read test image & process it
-	image_file = "test/300000.jpg"
+    
+
+	image_file = input('Enter the name of image present in test folder in format: test/image_name.jpg\n')
+    # read test image & process it
+	# example of image_file = "test/300000.jpg"
 	print("Test Image is being read and vectorized!")
 	eigen_vec=[]
 	im = cv2.imread(image_file)
